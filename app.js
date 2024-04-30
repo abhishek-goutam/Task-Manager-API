a465296e961d2fc28734cc44e21fe57b1a4710d3
Abhishek Goutam <abhishekgoutam8894@gmail.com>
2024-04-30 13:31:34 +0530

    task manager api

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+var tasks = require('./task.json').tasks;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const tasks = [];
 let taskId = 0;
 
 const validateInput = (req, res, next) => {
